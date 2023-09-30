7adc45521a4550feca08aed44ed49668f89efcb4
klemenslatnar <klemen.slatnar06@gmail.com>
2023-09-30 19:54:03 +0200

    Gender product querying

--- src/components/Men.js
+++ src/components/Men.js
@@ -8,11 +8,14 @@ function Men() {
   const [products, setProducts] = useState([]);
 
   const productsList = products.map((product) => {
-    return <Product product={product} key={product.name} />;
+    return <Product product={product} key={product._id} />;
   });
 
   useEffect(() => {
-    axios("http://localhost:8000/get-products")
+    axios({
+      url: "http://localhost:8000/get-products",
+      params: { gender: "male" },
+    })
       .then((res) => {
         const dbProducts = res.data.products;
         setProducts(dbProducts);
--- src/components/Women.js
+++ src/components/Women.js
@@ -1,9 +1,32 @@
-import "./Women.css"
+import "./Women.css";
+import Product from "./Product";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
 
 function Women() {
-  return (
-   <div>Women</div>
-  )
+  const [products, setProducts] = useState([]);
+
+  const productsList = products.map((product) => {
+    return <Product product={product} key={product.name} />;
+  });
+
+  useEffect(() => {
+    axios({
+      url: "http://localhost:8000/get-products",
+      params: { gender: "female" },
+    })
+      .then((res) => {
+        const dbProducts = res.data.products;
+        setProducts(dbProducts);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <>{productsList}</>;
 }
 
-export default Women
+export default Women;
