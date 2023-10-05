7592e2a2d9df9b8e8bd506780b1a4a0adbf13707
klemenslatnar <klemen.slatnar06@gmail.com>
2023-10-05 19:50:03 +0200

    Cart Page Added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,49 @@
-import "./Cart.css"
+import "./Cart.css";
+import CartItem from "./CartItem";
 
 function Cart() {
+  const checkoutHandler = () => {
+    console.log("Checkout");
+  };
+
   return (
-    <div className="container">Cart</div>
-  )
+    <>
+      <h1 className="cart-heading">Cart</h1>
+      <div className="grey-line"></div>
+      <div className="cart-items-container">
+        <CartItem
+          cartItem={{
+            name: "Boots",
+            id: "1",
+            quantity: 2,
+            price: 99.99,
+            size: "L",
+          }}
+        />{" "}
+        <CartItem
+          cartItem={{
+            name: "Boots",
+            id: "1",
+            quantity: 2,
+            price: 99.99,
+            size: "L",
+          }}
+        />{" "}
+        <CartItem
+          cartItem={{
+            name: "Boots",
+            id: "1",
+            quantity: 2,
+            price: 99.99,
+            size: "L",
+          }}
+        />
+      </div>
+      <div className="checkout">
+        <button onClick={checkoutHandler}>Checkout</button>
+      </div>
+    </>
+  );
 }
 
-export default Cart
+export default Cart;
---
+++ src/components/CartItem.js
@@ -0,0 +1,34 @@
+import "./CartItem.css";
+import bootImage from "../assets/black-leather-boots-2.jpg";
+import iconX from "../assets/menu-x.png";
+
+function CartItem({ cartItem }) {
+    const removeHandler = () => {
+      console.log("Removed")
+  };
+
+  return (
+    <>
+      <div className="cart-item">
+        <div className="cart-item-image-wrapper">
+          <img className="cart-item-image" src={bootImage} alt="Cart Item" />
+        </div>
+        <div className="cart-item-info">
+          <h1>{cartItem.name}</h1>
+          <p>Size: {cartItem.size}</p>
+          <p>Quantity: {cartItem.quantity}</p>
+          <p>£{cartItem.price}</p>
+        </div>
+        <img
+          className="remove-item"
+          src={iconX}
+          alt="Remove Item"
+          onClick={removeHandler}
+        />
+      </div>
+      <div className="grey-line"></div>
+    </>
+  );
+}
+
+export default CartItem;
